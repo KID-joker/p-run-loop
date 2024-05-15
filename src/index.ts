c5821f05b7d3b7872b87e7a5bcaaa3a76fae9fd2
KID-joker <kid_joker@126.com>
2024-05-15 22:01:09 +0800

    feat: control promise order for shared resources

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,35 @@
-export const one = 1;
-export const two = 2;
+export default class PNext {
+	#queue: Function[] = [];
+	#map: WeakMap<Function, unknown> = new WeakMap();
+	#queuing: Boolean = false;
+
+	add(fn: Function) {
+		let p = this.#map.get(fn);
+		if (p) {
+			return p;
+		}
+		p = new Proxy(fn, {
+			apply: (target, thisArg, argArray) => {
+				if (this.#queuing) {
+					return new Promise((resolve) => {
+						this.#queue.push(resolve);
+					}).then(() => Reflect.apply(target, thisArg, argArray));
+				} else {
+					this.#queuing = true;
+					return Reflect.apply(target, thisArg, argArray);
+				}
+			},
+		});
+		this.#map.set(fn, p);
+		return p;
+	}
+
+	next() {
+		if (this.#queue.length > 0) {
+			const resolve = this.#queue.shift();
+			resolve!();
+		} else {
+			this.#queuing = false;
+		}
+	}
+}
